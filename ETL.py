aa79664eb1f5c904d2312f71e9f4b53901d54516
Jr <jr.jimeno@me.com>
2024-03-20 21:28:01 -0400

    more commiting

--- ETL.py
+++ ETL.py
@@ -3,14 +3,15 @@ import requests
 import json
 url = "https://itunes.apple.com/search?term=thor&media=movie"
 response = requests.get(url)
-print(type(response))
-#Verifying that the connection is OK (expected code 200)
-# print(f"Status is {response.status_code}")
 
-# #Loading
-# json_obj = json.loads(response.text)
-# results_array = json_obj["results"]
-# for result in results_array:
-#     runtime = (result["trackTimeMillis"] / 3600000)
-#     movie_name = result["trackName"]
-#     print(f"Movie:: {movie_name} - Runtime:: {round(runtime,1)}h")
+#Verifying that the connection is OK (expected code 200)
+print(f"Status is {response.status_code}")
+
+#Loading the downloaded text as a json string
+json_obj = json.loads(response.text)
+#We want to look into the _results_ key from the file 
+results_array = json_obj["results"]
+for result in results_array:
+    runtime = (result["trackTimeMillis"] / 3600000) #Transforming the time from miliseconds to hours so we can get a proper runtime
+    movie_name = result["trackName"] #The key _trackName_ contains the movie
+    print(f"Movie:: {movie_name} - Runtime:: {round(runtime,1)}h")
