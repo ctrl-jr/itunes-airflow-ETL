79c4d0cab9c812b2f3aa81575570fdfa9c534f1a
Jr <jr.jimeno@me.com>
2024-03-20 21:21:01 -0400

    commiting!

--- ETL.py
+++ ETL.py
@@ -3,4 +3,14 @@ import requests
 import json
 url = "https://itunes.apple.com/search?term=thor&media=movie"
 response = requests.get(url)
-print(f"Status is {response.status_code}")
+print(type(response))
+#Verifying that the connection is OK (expected code 200)
+# print(f"Status is {response.status_code}")
+
+# #Loading
+# json_obj = json.loads(response.text)
+# results_array = json_obj["results"]
+# for result in results_array:
+#     runtime = (result["trackTimeMillis"] / 3600000)
+#     movie_name = result["trackName"]
+#     print(f"Movie:: {movie_name} - Runtime:: {round(runtime,1)}h")
