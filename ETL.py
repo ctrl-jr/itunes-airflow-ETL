3f9b10aa8aa6a5f6d20495e16ea60e0769a6ae68
Jr <jr.jimeno@me.com>
2024-03-20 21:07:15 -0400

    ...

--- ETL.py
+++ ETL.py
@@ -1,1 +1,6 @@
 #Step 1 : pull data from iTunes
+import requests
+import json
+url = "https://itunes.apple.com/search?term=thor&media=movie"
+response = requests.get(url)
+print(f"Status is {response.status_code}")
