3df462961e3770a876616465bc5c55894f6e9d79
Jr <jr.jimeno@me.com>
2024-03-20 22:31:30 -0400

    created the list with runtime, name and genre

--- ETL.py
+++ ETL.py
@@ -1,17 +1,29 @@
 #Step 1 : pull data from iTunes
 import requests
 import json
-url = "https://itunes.apple.com/search?term=thor&media=movie"
+import pandas as pd
+url = "https://itunes.apple.com/search?term=avengers&media=movie"
 response = requests.get(url)
 
 #Verifying that the connection is OK (expected code 200)
-print(f"Status is {response.status_code}")
+print(f"Status is {response.status_code} !")
 
 #Loading the downloaded text as a json string
 json_obj = json.loads(response.text)
 #We want to look into the _results_ key from the file 
 results_array = json_obj["results"]
+movie_list = []
 for result in results_array:
-    runtime = (result["trackTimeMillis"] / 3600000) #Transforming the time from miliseconds to hours so we can get a proper runtime
-    movie_name = result["trackName"] #The key _trackName_ contains the movie
-    print(f"Movie:: {movie_name} - Runtime:: {round(runtime,1)}h")
+    refined_result = {
+        "runtime": (result["trackTimeMillis"] / 3600000), #Transforming the time from miliseconds to hours so we can get a proper runtime
+        "movie_name" : result["trackName"], #The key _trackName_ contains the movie
+        "movie_genre" : result["primaryGenreName"] }
+
+    movie_list.append(refined_result)
+
+    #print(f"Movie:: {movie_name}") #- Runtime:: {round(runtime,1)}h")
+print(movie_list)
+#Creating a dataframe of our move list
+df = pd.DataFrame(movie_list)
+#Creating a local CSV to view the list
+df.to_csv("avengers_movies.csv")
