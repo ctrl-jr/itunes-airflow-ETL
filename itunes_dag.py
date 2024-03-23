18288a267e217645fbc60f0d3f0c8bd244a70908
Jr <jr.jimeno@me.com>
2024-03-22 22:39:25 -0400

    updating dag.py file

--- itunes_dag.py
+++ itunes_dag.py
@@ -3,3 +3,30 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
+from ETL import run_itunes_etl
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 11, 8),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+    'itunes_dag',
+    default_args=default_args,
+    description='iTunes test DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+run_etl = PythonOperator(
+    task_id='complete_twitter_etl',
+    python_callable=run_itunes_etl, #just like our function in the ETL.py file
+    dag=dag, 
+)
+
+run_etl
