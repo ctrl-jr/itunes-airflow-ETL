fcf09889f29f538b1c9a9fa98c434adb51e31433
Jr <jr.jimeno@me.com>
2024-03-26 20:47:16 -0400

    added sqlite

--- airflow steps EC2.sh
+++ airflow steps EC2.sh
@@ -1,7 +1,8 @@
 #Steps for installing Airflow 2.8.3 on Ubuntu EC2 using Python 3.10
 sudo apt-get update
 sudo apt install python3-pip
-pip install "apache-airflow==2.8.3" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.8.3/constraints-no-providers-3.10.txt"
+sudo apt install sqlite3
+pip install "apache-airflow==2.8.4" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.8.4/constraints-no-providers-3.10.txt"
 PATH=$PATH:~/.local/bin
 
 airflow db init
