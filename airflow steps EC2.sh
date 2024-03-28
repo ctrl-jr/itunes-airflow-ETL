58890b9facddcbe338fe378878785b2f85a6dce9
Jr <jr.jimeno@me.com>
2024-03-27 22:38:47 -0400

    some edits on airflow steps

--- airflow steps EC2.sh
+++ airflow steps EC2.sh
@@ -1,9 +1,10 @@
-#Steps for installing Airflow 2.8.3 on Ubuntu EC2 using Python 3.10
+#Steps for installing and running Airflow 2.8.4 on Ubuntu EC2 using Python 3.10
 sudo apt-get update
 sudo apt install python3-pip
 sudo apt install sqlite3
-pip install "apache-airflow==2.8.4" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.8.4/constraints-no-providers-3.10.txt"
 PATH=$PATH:~/.local/bin
+pip install "apache-airflow==2.8.4" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.8.4/constraints-no-providers-3.10.txt"
+
 
 airflow db init
 airflow users create --username admin --password admin --firstname John --lastname Doe --role Admin --email admin@domain.com
